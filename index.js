beb19c572422df323b896dbb3c6efd0d14fcaec9
RafitJ <rafit.jamil@gmail.com>
2019-01-11 20:00:04 -0500

    Major animation updates, text changes and responsiveness fixes

--- index.js
+++ index.js
@@ -3,37 +3,46 @@
 //   $("h2").addClass("faded");
 // };
 
-$(".middle-box").hover(function(){
+AOS.init();
+
+AOS.init({
+  disable: function() {
+    var maxWidth = 900;
+    return window.innerWidth < maxWidth;
+  }
+});
+
+$(".middle-box").hover(function() {
   $('#passion-header').addClass('moveup');
 }, function() {
   $('#passion-header').removeClass('moveup');
 });
 
-$(".leg-exp").hover(function(){
+$(".leg-exp").hover(function() {
   $(this).siblings('.floating').addClass('shad-change');
 }, function() {
   $(this).siblings('.floating').removeClass('shad-change');
 });
 
-$(".leg-passion").hover(function(){
+$(".leg-passion").hover(function() {
   $(this).siblings('.floating').addClass('shad-change');
 }, function() {
   $(this).siblings('.floating').removeClass('shad-change');
 });
 
-$(".leg-skills").hover(function(){
+$(".leg-skills").hover(function() {
   $(this).siblings('.floating').addClass('shad-change');
 }, function() {
   $(this).siblings('.floating').removeClass('shad-change');
 });
 
-$(".leg-proj").hover(function(){
+$(".leg-proj").hover(function() {
   $(this).siblings('.floating').addClass('shad-change');
 }, function() {
   $(this).siblings('.floating').removeClass('shad-change');
 });
 
-$(".leg-chat").hover(function(){
+$(".leg-chat").hover(function() {
   $(this).siblings('.floating').addClass('shad-change');
 }, function() {
   $(this).siblings('.floating').removeClass('shad-change');
@@ -46,73 +55,80 @@ $("#lines").on({
   }
 });
 
+$("#inner-1").on({
+  click: function() {
+    $(this).toggleClass('flip');
+  }
+});
+$("#inner-2").on({
+  click: function() {
+    $(this).toggleClass('flip');
+  }
+});
+$("#inner-3").on({
+  click: function() {
+    $(this).toggleClass('flip');
+  }
+});
+
 $(document).ready(function() {
 
   var active0 = false;
-  var active1 = false;
-  var active2 = false;
-  var active3 = false;
-  var active4 = false;
-  var active5 = false;
 
   $('.parent2').on('click', function() {
-
-    if (!active0) $(this).find('.test0').css({
-      'transform': 'translate(0px,100px)',
-      'opacity': '1'
-    });
-    else $(this).find('.test0').css({
-      'transform': 'none',
-      'opacity': '0'
-    });
-
-    if (!active1) $(this).find('.test1').css({
-      'transform': 'translate(0px,180px)',
-      'opacity': '1'
-    });
-    else $(this).find('.test1').css({
-      'transform': 'none',
-      'opacity': '0'
-    });
-    if (!active2) $(this).find('.test2').css({
-      'transform': 'translate(0px,260px)',
-      'opacity': '1'
-    });
-    else $(this).find('.test2').css({
-      'transform': 'none',
-      'opacity': '0'
-    });
-    if (!active3) $(this).find('.test3').css({
-      'transform': 'translate(0px,340px)',
-      'opacity': '1'
-    });
-    else $(this).find('.test3').css({
-      'transform': 'none',
-      'opacity': '0'
-    });
-    if (!active4) $(this).find('.test4').css({
-      'transform': 'translate(0px,420px)',
-      'opacity': '1'
-    });
-    else $(this).find('.test4').css({
-      'transform': 'none',
-      'opacity': '0'
-    });
-    if (!active4) $(this).find('.test5').css({
-      'transform': 'translate(0px,500px)',
-      'opacity': '1'
-    });
-    else $(this).find('.test5').css({
-      'transform': 'none',
-      'opacity': '0'
-    });
+    if (!active0) {
+      $(this).find('.test0').css({
+        'transform': 'translate(0px,100px)',
+        'opacity': '1'
+      })
+      $(this).find('.test1').css({
+        'transform': 'translate(0px,180px)',
+        'opacity': '1'
+      });
+      $(this).find('.test2').css({
+        'transform': 'translate(0px,260px)',
+        'opacity': '1'
+      });
+      $(this).find('.test3').css({
+        'transform': 'translate(0px,340px)',
+        'opacity': '1'
+      });
+      $(this).find('.test4').css({
+        'transform': 'translate(0px,420px)',
+        'opacity': '1'
+      });
+      $(this).find('.test5').css({
+        'transform': 'translate(0px,500px)',
+        'opacity': '1'
+      });
+    } else {
+      $(this).find('.test0').css({
+        'transform': 'none',
+        'opacity': '0'
+      });
+      $(this).find('.test1').css({
+        'transform': 'none',
+        'opacity': '0'
+      });
+      $(this).find('.test2').css({
+        'transform': 'none',
+        'opacity': '0'
+      });
+      $(this).find('.test4').css({
+        'transform': 'none',
+        'opacity': '0'
+      });
+      $(this).find('.test3').css({
+        'transform': 'none',
+        'opacity': '0'
+      });
+      $(this).find('.test5').css({
+        'transform': 'none',
+        'opacity': '0'
+      });
+    }
 
     active0 = !active0;
-    active1 = !active1;
-    active2 = !active2;
-    active3 = !active3;
-    active4 = !active4;
-    active5 = !active5;
 
   });
 });
@@ -129,4 +145,50 @@ var myScrollFunc = function() {
   }
 };
 
-window.addEventListener("scroll", myScrollFunc);
+window.addEventListener("scroll", myScrollFunc)
+
+
+
+
+
+
+
+
+$(function() {
+
+  var $meters = $(".meter > span");
+  var $section = $('#skills');
+  var $queue = $({});
+
+  function loadDaBars() {
+    $meters.each(function() {
+      var $el = $(this);
+      var origWidth = $el.width();
+      $el.width(0);
+      $queue.queue(function(next) {
+        $el.animate({
+          width: origWidth
+        }, 300, next);
+      });
+    });
+  }
+
+  $(document).bind('scroll', function(ev) {
+    var scrollOffset = $(document).scrollTop();
+    var containerOffset = $section.offset().top - window.innerHeight;
+    if (scrollOffset > containerOffset) {
+      loadDaBars();
+      // unbind event not to load scrolsl again
+      $(document).unbind('scroll');
+    }
+  });
+
+});
+
+
+
+$(".contact-item").hover(function() {
+  $(".contact-item > .para2").addClass('fade-down');
+}, function() {
+  $(".contact-item > .para2").removeClass('fade-down');
+});
