5e81e0a747a7499ff7e3ad8064e524c189c46fed
RafitJ <rafit.jamil@gmail.com>
2019-07-26 22:08:04 -0400

    Updates

---
+++ precache-manifest.629b5186ee2f106b9330bea36ac52a04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66754fd192c019ef0be3d6905572ea37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "698178326c81bac59cb1",
+    "url": "/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "e19323175019dbb401d4",
+    "url": "/static/css/main.c918cc0b.chunk.css"
+  },
+  {
+    "revision": "698178326c81bac59cb1",
+    "url": "/static/js/2.917f467c.chunk.js"
+  },
+  {
+    "revision": "e19323175019dbb401d4",
+    "url": "/static/js/main.9dc85e94.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "445ea13643f713480acbd0cdbe463a5b",
+    "url": "/static/media/Resume.445ea136.pdf"
+  }
+]);
