53bc1fd3559f85839b2033a6f186045a7b8e1eac
RafitJ <rafit.jamil@gmail.com>
2019-09-07 03:03:08 -0400

    white version

--- src/components/Sections/Projects/ProjectCard.js
+++ src/components/Sections/Projects/ProjectCard.js
@@ -14,7 +14,7 @@ class ProjectCard extends Component {
     render() {
         const classes = "project_card " + this.props.extra_classes
         return (
-            <Col sm ={12} md ={6} xl ={4}>
+            <Col sm ={12} md ={12} xl ={12}>
             <div className = {classes} data-aos="fade-up" data-aos-duration = "500">
                 <div className = "project_logo">
                     <img alt = {this.props.title} src = {this.props.img} />
